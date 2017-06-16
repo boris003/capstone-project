1c5593e8939aa040a539b57094aa61c2fb97a732
bdanilin <danilin003@gmail.com>
2017-06-15 21:10:25 -0700

    added html sign-up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+  def index
+    #just temporary page
+    render "index.html.erb"
+  end
+
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    user = User.new(
+      nickname: params[:nickname],
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      date_of_birth: params[:date_of_birth],
+      location: params[:location],
+      drone_model: params[:drone_model],
+      bio: params[:bio],
+    )
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully created account!'
+      redirect_to '/'
+    else
+      flash[:warning] = 'Invalid email or password!'
+      redirect_to '/signup'
+    end
+  end
 end
